f2b1bde4775edcda95435d1b3076f666e95d9b7b
VASATHE <vaishnavisathe9500@gmail.com>
2025-12-06 12:22:50 +0530

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module GO_language
+module Day2
 
 go 1.25.5
--- hello.go
+++ hello.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var name = "World"
+
 func hello() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("Hello, %v !", name)
 }
